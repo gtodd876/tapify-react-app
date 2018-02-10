5de6efd4dc6a1588f128aed3a958795de658e976
Todd Matthews <gtmatthews@gmail.com>
2018-02-10 09:59:25 -0500

    Change login button to point to heroku backend

--- src/components/SpotifyButton/SpotifyButton.js
+++ src/components/SpotifyButton/SpotifyButton.js
@@ -3,7 +3,7 @@ import './SpotifyButton.css';
 
 const SpotifyButton = () => (
   <button
-    onClick={() => (window.location = process.env.BACKEND_URI || 'http://localhost:8888/login')}
+    onClick={() => (window.location = 'https://limitless-woodland-67549.herokuapp.com/login')}
     className="login"
     id="login-button"
   >
