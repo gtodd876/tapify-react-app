dbc473258884ad031a565be1ee3bda9ae1835340
Todd Matthews <gtmatthews@gmail.com>
2018-02-12 08:35:12 -0500

    Revise text instructions to fit on mobile screen

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -4,15 +4,10 @@ import './Instructions.css';
 const Instructions = () => (
   <Fragment>
     <p className="instructions">
-      Tap your desired beat on the screen or with the<span>spacebar</span> to become a human metronome. Tapify will
-      calculate the tempo of your taps in BPM. (Beats Per Minute)
+      Tap your desired beat on the screen or with the<span>spacebar</span> to become a human metronome.
     </p>
     <p className="instructions">
       Press <span>r</span> key to reset tempo and start over
-    </p>
-
-    <p className="instructions">
-      After tempo is displayed, tap the submit button to fetch a playlist of songs at your tempo!
     </p>
   </Fragment>
 );
