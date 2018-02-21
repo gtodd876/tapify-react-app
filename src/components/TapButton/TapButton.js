c2a40343b86bd4e254f8c44849a1dea9c70c4cea
Todd Matthews <gtmatthews@gmail.com>
2018-02-20 19:16:09 -0500

    Add onClick to TapButton

--- src/components/TapButton/TapButton.js
+++ src/components/TapButton/TapButton.js
@@ -3,14 +3,14 @@ import './TapButton.css';
 
 const TapButton = props => (
   <div className="click-me">
-    <button className="tap-btn" onTouchStart={() => props.tempoTouch()}>
+    <button
+      className="tap-btn"
+      onClick={() => props.tempoTouch()}
+      onTouchStart={() => props.tempoTouch()}
+    >
       Tap
     </button>
   </div>
 );
 
 export default TapButton;
-
-//onClick={() => props.tempoLogic()}
-
-//
