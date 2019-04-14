5412f3e1ef6af0d36a3fd44491dd0b9e7538d632
Todd Matthews <gtmatthews@gmail.com>
2019-04-13 21:11:27 -0400

    Add explicit converstion of str to int

--- src/components/BpmDisplay/BpmDisplay.js
+++ src/components/BpmDisplay/BpmDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './BpmDisplay.css';
 import PropTypes from 'prop-types';
 
-const BpmDisplay = ({ tempo }) => <p className="bpm">{Math.floor(tempo)} BPM</p>;
+const BpmDisplay = ({ tempo }) => <p className="bpm">{Math.floor(parseInt(tempo))} BPM</p>;
 
 BpmDisplay.propTypes = {
   tempo: PropTypes.string
